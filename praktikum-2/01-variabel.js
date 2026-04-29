284df0885724334bc1fc05cb68e0f7af6aefa6ae
msyarfani <msyarfani233@gmail.com>
2026-04-29 09:05:31 +0700

    Praktikum-2 : Dasar javascript - variabel, Tipe data, Operator, Kondisional, Perulangan

--- praktikum-2/01-variabel.js
+++ praktikum-2/01-variabel.js
@@ -1,15 +1,15 @@
-let nama = 'Budi';
+let nama = "Budi";
 let umur = 20;
-let kota = 'Banda Aceh';
-console.log('=== Data Mahasiswa ===');
-console.log('Nama :', nama);
-console.log('Umur :', umur);
-console.log('Kota :', kota);
+let kota = "Banda Aceh";
+console.log("=== Data Mahasiswa ===");
+console.log("Nama :", nama);
+console.log("Umur :", umur);
+console.log("Kota :", kota);
 
 umur = 21;
-console.log('Ulang tahun! Umur sekarang:', umur);
+console.log("Ulang tahun! Umur sekarang:", umur);
 
-const jurusan = 'Teknik Informatika';
+const jurusan = "Teknik Informatika";
 const tahunMasuk = 2023;
-console.log('Jurusan :', jurusan);
-console.log('Tahun Masuk:', tahunMasuk);
+console.log("Jurusan :", jurusan);
+console.log("Tahun Masuk:", tahunMasuk);
